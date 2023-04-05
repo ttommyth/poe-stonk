9437af27b9254af8d2cc8ba8be7dc1cb060ab833
ttommyth <ttommyth@gmail.com>
2023-04-05 15:15:03 +0800

    feat: added previous league in selector

--- src/components/client/LeagueSelector.tsx
+++ src/components/client/LeagueSelector.tsx
@@ -3,19 +3,35 @@
 import { League } from "@/libs/fetchNinja"
 import { usePathname, useRouter } from "next/navigation";
 import { useMemo } from "react";
-import Select from "react-select";
+import Select, { GroupHeadingProps } from "react-select";
 
 export const LeagueSelector=(props:{
   leagues:League[]
+  oldLeagues:League[]
 })=>{
-  const {leagues} = props;
+  const {leagues, oldLeagues} = props;
   const router = useRouter();
   const pathname = usePathname();
   const [selectedLeagueName] = pathname?.split("/")??[];
-  const selectedLeague = useMemo(()=>leagues.find(it=>it.name===selectedLeagueName), [leagues, selectedLeagueName])
-  return <Select options={leagues} value={selectedLeague}
-    className="[*>input]:daisy-select w-full max-w-xs"
-    getOptionLabel={it=>it.displayName}
-    getOptionValue={it=>it.name}
-    onChange={v=>router.push(`/${v?.name??""}`)} />
+  const mappedLeague =useMemo(()=>leagues.map(it=>({
+    value: it.name,
+    label: it.displayName
+  })??[]),[leagues])
+  const mappedOldLeague =useMemo(()=>oldLeagues.map(it=>({
+    value: it.name,
+    label: it.displayName
+  }))??[],[oldLeagues])
+  // const selectedLeague = useMemo(()=>leagues.find(it=>it.name===selectedLeagueName), [leagues, selectedLeagueName])
+  return <Select options={[
+    {
+      label:"Current Leagues",
+      options: mappedLeague
+    },
+    {
+      label:"Previous Leagues",
+      options: mappedOldLeague
+    },
+  ]} value={mappedLeague.find(it=>it.value==selectedLeagueName) ?? mappedLeague.find(it=>it.value==selectedLeagueName) }
+  className="[*>input]:daisy-select w-full max-w-xs"
+  onChange={v=>router.push(`/${v?.value??""}`)} />
 }
